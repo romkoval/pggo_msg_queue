0eeca60cc5587a8c1e0d45d4b153821edf9a804c
Roman Kovalev <rom@sirena2000.ru>
2021-09-19 11:17:40 +0300

    schema upd

--- schema.sql
+++ schema.sql
@@ -2,7 +2,7 @@ drop table if exists msg_queue;
 create table msg_queue(
     msg_id integer not null,
     queue_type varchar(10) not null, -- type1, type2 ...
-    status varchar(1) not null, -- N (new), P (process), D (done)
+    status varchar(1) not null, -- N (new), P (processed)
     order_id integer not null, -- records with same order_id and queue_type have to be handled in correct order, 1,2,3,4...
     time_added timestamp with time zone not null,
     time_processed timestamp with time zone
